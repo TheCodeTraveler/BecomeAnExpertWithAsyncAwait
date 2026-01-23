474cd67a7b79ff211437867da9ae4f98401307be
Brandon Minnick <13558917+TheCodeTraveler@users.noreply.github.com>
2026-01-23 11:57:27 -0800

    Add Start

---
+++ 3. Creating Custom Implementation of Task/1. Start/CreatingTaskFromScratch/CustomTask.cs
@@ -0,0 +1,8 @@
+using System.Runtime.CompilerServices;
+using System.Runtime.ExceptionServices;
+
+namespace CreatingTaskFromScratch;
+
+sealed class CustomTask
+{
+}
---
+++ 3. Creating Custom Implementation of Task/1. Start/CreatingTaskFromScratch/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CreatingTaskFromScratch;
+
+Console.WriteLine($"Starting Thread Id: {Environment.CurrentManagedThreadId}");
